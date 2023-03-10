2b6bbab423279a1cbdefa1d1d4d50fc55df4b979
TomNdPop <lilleymanryan2020@gmail.com>
2023-03-10 14:33:48 -0500

    Fix the various bugs with console.log

--- rock-paper-scissors/index.js
+++ rock-paper-scissors/index.js
@@ -4,92 +4,99 @@
 function playerSelection(){
 
     //Initialize string as falsy.
-    playerSelect = ''
+    var playerSelect = '';
 
     //Prompt user for rock, paper, or scissors
     // and then convert the string as to help player out with valid input.
-    playerSelect = prompt("Enter Rock, Paper or Scissors.")
-    playerSelect = playerSelect.toUpperCase()
-    playerSelect = playerSelect[0]
+        playerSelect = prompt("Enter Rock, Paper or Scissors.");
+        playerSelect = playerSelect.toUpperCase();
+        playerSelect = playerSelect[0];
 
     //Correct the string for the user and for future function use.
     if (playerSelect =="R"){
-        playerSelect = 0
+        playerSelect = 0;
     }else if(playerSelect =="P"){
-        playerSelect = 1
+        playerSelect = 1;
     }else if(playerSelect =="S"){
-        playerSelect = 2
+        playerSelect = 2;
     }
 
-    return playerSelect
+    return playerSelect;
 }
 
 //Random integer function
-function getRandInt(max){
-    return Math.floor(Math.random()*max)
+function getRandomInt(max) {
+    return Math.floor(Math.random()*max);
 }
 
 //Computer selection function
 function computerSelection(){
-    test = getRandInt(3)
-    return test
+    let test = getRandomInt(3);
+    return test;
 }
 
 function game(){
     //I use 0 = Rock, 1 = Paper, and 2 = Scissors
-    yWin = 0
-    cWin = 0
-    tie = 0
-
-    while (yWin !== 4 || cWin !== 4){
-        pSelect = playerSelection()
-        cSelect = computerSelection()
+    let yWin = 0;
+    let cWin = 0;
+    let tie = 0;
+    let repeat = 1;
+    while (repeat = 1){
+        let pSelect = playerSelection();
+        console.log(pSelect);
+        let cSelect = computerSelection();
+        console.log(cSelect);
         if(pSelect ==2){
             if(cSelect==1){
-                ++yWin
-                console.log(yWin,"You Won! Scissors beats Paper.")
-                continue
+                ++yWin;
+                console.log(yWin,"You Won! Scissors beats Paper.");
             }else if(cSelect==0){
-                ++cWin
-                console.log(cWin,"You Lost! Rock beats Scissors.")
-                continue
+                ++cWin;
+                console.log(cWin,"You Lost! Rock beats Scissors.");
+            }else if(cSelect==2){
+                ++tie;
+                console.log("That was a Tie!");
             }
         }else if(pSelect==1){
             if(cSelect==2){
-                ++cWin
-                console.log(cWin,"You Lost! Scissors beats Paper.")
-                continue
+                ++cWin;
+                console.log(cWin,"You Lost! Scissors beats Paper.");
             }else if(cSelect==0){
-                ++yWin
-                console.log(yWin,"You Won! Paper beats Rock.")
-                continue
+                ++yWin;
+                console.log(yWin,"You Won! Paper beats Rock.");
+            }else if(cSelect==2){
+                ++tie;
+                console.log("That was a Tie!");
             }
         }else if(pSelect==0){
             if(cSelect==1){
-                ++cWin
-                console.log(cWin,"You Lost! Paper beats Rock.")
-                continue
+                ++cWin;
+                console.log(cWin,"You Lost! Paper beats Rock.");
             }else if(cSelect==2){
-                ++yWin
-                console.log(yWin,"You Won! Rock beats Scissors.")
-                continue
+                ++yWin;
+                console.log(yWin,"You Won! Rock beats Scissors.");
+            }else if(cSelect==2){
+                ++tie;
+                console.log("That was a Tie!");
             }
+        }
+        if(yWin == 5 || cWin == 5){
+            repeat = 0;
+            break
         }else{
-            ++tie
-            console.log("That was a tie!")
-            continue
+            repeat = 1;
         }
     }
     if(yWin>cWin){
-        console.log("You Win!")
+        console.log("You Win!");
     }else if(cWin>yWin){
-        console.log("You Lost!")
+        console.log("You Lost!");
     }
 
 }
 
 function Main(){
-    game()
+    game();
 }
 
-Main()
+Main();
